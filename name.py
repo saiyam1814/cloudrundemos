07e2f9f364211012356c197d6d60703ea39d0d88
Saiyam Pathak <saiyam911@gmail.com>
2020-07-19 10:28:09 +0530

    Create name.py

---
+++ name.py
@@ -0,0 +1,16 @@
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+    return render_template('name.html')
+
+@app.route('/howdy', methods=['POST'])
+def howdy():
+    name = request.form['name']
+    return """
+    <h1>Howdy<h1>
+    <h1>{}<h1>
+    """.format(name)
+
+if __name__ == '__main__':
+    app.run(host = '0.0.0.0', port = 3000)
